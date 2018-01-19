1d7873bcdcbb66866dfa73545987edd6450f33d9
kassiacatarine <kassia_catarine.15@hotmail.com>
2018-01-19 14:09:58 -0200

    Adição do escopo inicial da calculadora
    Signed-off-by: kassiacatarine <kassia_catarine.15@hotmail.com>

--- treinamento-1/Javascript/parte-1/calculadora.js
+++ treinamento-1/Javascript/parte-1/calculadora.js
@@ -1,75 +1,148 @@
 let values = [];// = [10, 3, 4, 3];
 let roles = []; //= ['+', '-', '+'];
-
-// VIEWS
-
-
-
-function printExpression(values, roles) {
-    let cont = 0;
-    let expression = "";
-    
-    if(!arrayEmpty()){
-        do {
-            if(values.length - 1 == cont){
-                expression += values[cont];
-            }else{
-                expression += values[cont] + ' ' +  roles[cont] + ' ';
-            }
-            cont++;
-        } while (cont < values.length);
-        console.log(expression);
-    }
-}
-
+let number;
 
 // VALIDATIONS
 
 
-function arrayEmpty() {
-    values || roles ? alert('Expressão vazia, impossível imprimir!') : console.log('Expressão Impressa');
+function numberIsEmpty() {
+    if ((number == null || number == undefined || number == 0)) {
+        return true;
+    }
+    return false;
+}
+
+function arrayIsEmpty() {
+    if (values.length != 0 && roles.length != 0){
+        return false;
+    }
+    return true;
+}
+
+
+// EVENTS
+
+
+window.onload = clearOperation();
+
+
+// EVENTS LISTENERS --- BUTTONS NUMBERS
+
+
+let buttonsNumbers = document.getElementsByClassName('btn-number');
+
+for (const btn of buttonsNumbers) {
+    btn.addEventListener('click', elementClicked);
+}
+
+
+function elementClicked() {
+
+    if (numberIsEmpty() && this.value == '='){
+        alert("Operação Inválida! Tente Adicionar um número!");
+    }else{
+        if (!numberIsEmpty() && this.value == '='){
+            values.push(number);
+            calculateExpression();
+        } else {
+            if(numberIsEmpty() && this.value != '='){
+                number = this.value;
+            }else if (!numberIsEmpty() && this.value != '=') {
+                number += this.value;
+            }
+            printExpression();
+        }
+    }
+}
+
+
+// EVENT LISTENERS --- BUTTONS OPERATIONS
+
+
+let buttonsOperations = document.getElementsByClassName('btn-function');
+
+for (const btn of buttonsOperations) {
+    btn.addEventListener('click', pushOperation);
+}
+
+
+// DISPLAY
+
+
+function displayPrimary(item) {
+    document.getElementById('display-primary').innerText = item;
+}
+
+function displaySecondary(item) {
+    document.getElementById('display-secondary').innerText = item;
+}
+
+
+// VIEWS
+
+
+function printExpression() {
+    let expression = "";
+
+    if (!arrayIsEmpty()) {
+        for (let i = 0; i < values.length; i++) {
+            const e = values[i];
+            i < roles.length ? expression += e + ' ' + roles[i] + ' ' : expression += e;
+        }
+        if (!numberIsEmpty()) {
+            displaySecondary(expression + number);
+        } else {
+            displaySecondary(expression);
+        }
+    } else {
+        displaySecondary(number);
+    }
 }
 
 
 //FUNCTIONS
 
 
-function pushOperation(number, role) {
-    if (role == '=') {
-        values.push(number);
-        calculateExpression(values, roles);
-    } else {
-        values.push(number);
-        roles.push(role);
-    }
+function pushOperation() {
+    document.getElementById('display-primary').innerHTML = '<br>';
+    values.push(number);
+    roles.push(this.value);
+    number = 0;
+    printExpression();
 }
+
 
 function clearOperation() {
     values.length = 0;
     roles.length = 0;
+    number = 0;
+    document.getElementById('display-primary').innerHTML = '<br>';
+    document.getElementById('display-secondary').innerHTML = '<br>';
 }
 
 
 // CALCULATION
 
 
-function calculateExpression(values, roles) {
-    let cont = 0;
+function calculateExpression() {
 
     do {
         let operator = roles.shift();
+        
         if(operator == '+'){
-            values.unshift(sum(values.shift(), values.shift()));
+            values.unshift(sum(parseInt(values.shift()), parseInt(values.shift())));
         } else if (operator == '-') {
-            values.unshift(subtraction(values.shift(), values.shift()));
+            values.unshift(subtraction(parseInt(values.shift()), parseInt(values.shift())));
         } else if (operator == '*') {
-            values.unshift(multiplication(values.shift(), values.shift()));
+            values.unshift(multiplication(parseInt(values.shift()), parseInt(values.shift())));
         } else if (operator == '/') {
-            values.unshift(division(values.shift(), values.shift()));
+            values.unshift(division(parseInt(values.shift()), parseInt(values.shift())));
         }
     } while (roles.length != 0);
-
-    return values[0];
+    
+    displayPrimary('= ' + values[0]);
+    number = parseInt(values[0]);
+    values.length = 0;
 }
 
 
@@ -91,11 +164,3 @@ function multiplication(first, second) {
 function division(first, second) {
     return first / second;
 }
-
-
-
-pushOperation(1, '+');
-pushOperation(2, '+');
-pushOperation(2, '=');
-
-printExpression(values, roles);
